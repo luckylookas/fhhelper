596c017715ad526be7cbeee0eaecc1540d0a34ab
Lukas Noll <lukas.noll@untis.at>
2024-02-01 16:19:15 +0100

    increase town text size

--- src/App.tsx
+++ src/App.tsx
@@ -414,25 +414,25 @@ function App() {
                         <div className={'flex flex-col justify-center items-center min-w-full px-5'}>
                             <div className={'flex flex-col items-center min-w-full'}>
                                 <div className={'flex flex-auto flex-row text-sm'}>{town.name}</div>
-                                <div className={'flex flex-auto flex-row text-2xs'}>resources as ([propserity]) (wood)
+                                <div className={'flex flex-auto flex-row text-sm'}>resources as ([propserity]) (wood)
                                     (ore) (skin)
                                 </div>
-                                <div className={'flex flex-row justify-between w-full text-xs'}>
-                                    <div className={'text-xs'}>prosperity:</div>
-                                    <div className={'text-right text-xs'}>{town.prosperity}</div>
+                                <div className={'flex flex-row justify-between w-full text-sm'}>
+                                    <div className={'text-sm'}>prosperity:</div>
+                                    <div className={'text-right text-sm'}>{town.prosperity}</div>
                                 </div>
-                                <div className={'flex flex-row justify-between w-full text-xs'}>
-                                    <div className={'text-xs'}>morale:</div>
-                                    <div className={'text-right text-xs'}>{town.morale}</div>
+                                <div className={'flex flex-row justify-between w-full text-sm'}>
+                                    <div className={'text-sm'}>morale:</div>
+                                    <div className={'text-right text-sm'}>{town.morale}</div>
                                 </div>
-                                <div className={'flex flex-row justify-between w-full text-xs'}>
-                                    <div className={'text-xs'}>defense:</div>
+                                <div className={'flex flex-row justify-between w-full text-sm'}>
+                                    <div className={'text-sm'}>defense:</div>
                                     <div
-                                        className={'text-right text-xs'}>{mapMoraleToDefense(town.morale) + town.buildings?.filter(it => it.defenseBonus).map(it => it.defenseBonus).reduce((a: number, b: number) => a + b, 0)}</div>
+                                        className={'text-right text-sm'}>{mapMoraleToDefense(town.morale) + town.buildings?.filter(it => it.defenseBonus).map(it => it.defenseBonus).reduce((a: number, b: number) => a + b, 0)}</div>
                                 </div>
-                                <div className={'flex flex-row justify-between w-full text-xs'}>
-                                    <div className={'text-xs'}>soldiers:</div>
-                                    <div className={'text-right text-xs'}>{town.soldiers}</div>
+                                <div className={'flex flex-row justify-between w-full text-sm'}>
+                                    <div className={'text-sm'}>soldiers:</div>
+                                    <div className={'text-right text-sm'}>{town.soldiers}</div>
                                 </div>
                                 <div className={`pt-2 min-w-full`}>
                                     {town.buildings?.map(it => <div
@@ -446,25 +446,25 @@ function App() {
                                                 await wreck(it)
                                             }
                                         }}
-                                        className={`cursor-pointer min-h-6 max-h-6 min-w-full ${it.wrecked ? 'hover:bg-elementearth hover:text-ctext' : 'hover:bg-elementfire hover:text-ctext'} py-1 border-t-2 gap-1 flex flex-row border-highlight border-solid ${it.wrecked ? 'bg-elementfire text-ctext' : ''} justify-between`}>
+                                        className={`cursor-pointer min-h-6 min-w-full ${it.wrecked ? 'hover:bg-elementearth hover:text-ctext' : 'hover:bg-elementfire hover:text-ctext'} py-1 border-t-2 gap-1 flex flex-row border-highlight border-solid ${it.wrecked ? 'bg-elementfire text-ctext' : ''} justify-between`}>
                                         <div
-                                            className={'text-inherit bg-transparent text-2xs flex-auto basis-20 text-nowrap text-left font-bold'}>{it.name} ({it.level})
+                                            className={'text-inherit bg-transparent text-sm flex-auto basis-20 text-nowrap text-left font-bold'}>{it.name} ({it.level})
                                         </div>
                                         <div
-                                            className={'text-inherit bg-transparent text-2xs flex-auto basis-72 text-pretty text-left'}>{(it.wrecked && hoveringBuilding !== it) || (!it.wrecked && hoveringBuilding === it) ? (it.wreckedEffect ?? 'no effect') : it.effect}</div>
+                                            className={'text-inherit bg-transparent text-sm flex-auto basis-72 text-pretty text-left'}>{(it.wrecked && hoveringBuilding !== it) || (!it.wrecked && hoveringBuilding === it) ? (it.wreckedEffect ?? 'no effect') : it.effect}</div>
                                         <div
-                                            className={`text-inherit bg-transparent text-2xs flex-auto basis-10 text-nowrap text-left`}>
+                                            className={`text-inherit bg-transparent text-sm flex-auto basis-10 text-nowrap text-left`}>
                                             {it.wrecked && ['repair for ', ...it.repair].map(n =>
                                                 <span
-                                                    className={`text-inherit bg-transparent pe-1 text-2xs`}>{n}</span>)
+                                                    className={`text-inherit bg-transparent pe-1 text-sm`}>{n}</span>)
                                             }
                                             {!it.wrecked && it === hoveringBuilding &&
-                                                <span className={`text-inherit flex bg-transparent text-2xs text-left`}>
-                                                <span className={`text-inherit bg-transparent pe-1 text-2xs text-left`}>save for {it.instantRepairCost}</span>
+                                                <span className={`text-inherit flex bg-transparent text-sm text-left`}>
+                                                <span className={`text-inherit bg-transparent pe-1 text-sm text-left`}>save for {it.instantRepairCost}</span>
                                             </span>
                                             }
                                             {!it.wrecked && it.upgrade && it !== hoveringBuilding &&
-                                                <span className={`text-inherit flex bg-transparent text-2xs text-left`}>
+                                                <span className={`text-inherit flex bg-transparent text-sm text-left`}>
                                                         <span
                                                             className={`${!it.wrecked && it.upgrade && it.upgrade[0] > town.prosperity ? 'text-elementfire' : 'text-inherit'}  bg-transparent pe-1 text-2xs text-left`}>[{it.upgrade[0]}]</span>
                                                     {it.upgrade.slice(1).map(n => <span
@@ -472,7 +472,7 @@ function App() {
                                                     </span>
                                             }
                                             {!it.wrecked && !it.upgrade && it !== hoveringBuilding &&
-                                                <span className={'text-inherit bg-transparent pe-1 text-2xs'}></span>
+                                                <span className={'text-inherit bg-transparent pe-1 text-sm'}></span>
                                             }
                                         </div>
                                     </div>)}
